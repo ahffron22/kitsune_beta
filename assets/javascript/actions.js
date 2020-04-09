96c9b5aa786e492b8244a03d27be58d4469aa031
ahffron <ahffron22@gmail.com>
2020-04-08 22:45:01 -0600

    functioning paypal

--- assets/javascript/actions.js
+++ assets/javascript/actions.js
@@ -359,4 +359,35 @@ $(document).ready(function () {
       .getElementsByTagName("body")[0]
       .removeChild(document.getElementById("modalContainer"));
   }
+  // ----------------------------------------
+  // paypal
+
+  paypal
+    .Buttons({
+      style: {
+        shape: "pill",
+        color: "blue",
+        layout: "vertical",
+        label: "paypal",
+      },
+      createOrder: function (data, actions) {
+        return actions.order.create({
+          purchase_units: [
+            {
+              amount: {
+                value: taxTotal,
+              },
+            },
+          ],
+        });
+      },
+      onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+          alert(
+            "Transaction completed by " + details.payer.name.given_name + "!"
+          );
+        });
+      },
+    })
+    .render("#paypal-button-container");
 });
